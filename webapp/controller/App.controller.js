25f6d08b15dc9858b6f7c444117afea053a2db97
cuongtm95 <stmanhcuongtran@gmail.com>
2021-02-15 11:01:58 +0000

    Week 1 Unit 4

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+	], function(Controller, MessageToast) {
+	
+	Controller.extend("opensap.myapp.controller.App", {
+		onShowHello: function(){
+			MessageToast.show("Helow openSAP!");
+		}	
+	});
+});
