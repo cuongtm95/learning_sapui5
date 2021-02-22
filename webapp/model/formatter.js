4d402eb500da0f1632e5978bee0577cb0d054ae7
Tran Manh Cuong <stmanhcuongtran@gmail.com>
2021-02-22 10:35:22 +0000

    Unit 4: Adapting to the User's Device - Add content to object view (supplier, product, map) - hide the map on phones - Make Panels Expandable/Collapsible on Phones, using the Device Mode

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -14,6 +14,24 @@ sap.ui.define([], function () {
 				return "";
 			}
 			return parseFloat(sValue).toFixed(2);
+		},
+		
+		/**
+			 * Formats an address to a static google maps image
+			 * @public
+			 * @param {string} sStreet the street
+			 * @param {string} sZIPcode the postal code
+			 * @param {string} sCity the city
+			 * @param {string} sCountry the country
+			 * @returns {string} sValue a google maps URL that can be bound to an image
+			 */
+
+		formatMapUrl: function(sStreet, sZipcode, sCity, sCountry) {
+			
+			var address =  jQuery.sap.encodeURL(sStreet + "," + sZipcode +  "," + sCity + "," + sCountry),
+				myAPI = "&key=AIzaSyBWyJGCfBjXsoTG99nLQdyqB1CZfs6vqv4";
+			
+			return "https://maps.googleapis.com/maps/api/staticmap?center=" + address + "&size=600x600&&zoom=15&markers=color:red%7C" + address + myAPI;
 		}
 
 	};
